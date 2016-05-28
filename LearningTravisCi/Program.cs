726d1d7026c99157b21db34c95cd9f6e84089382
KZ <zhiping72@gmail.com>
2016-05-28 23:29:59 +0200

    Init

---
+++ LearningTravisCi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LearningTravisCi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
